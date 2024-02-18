562e7f71a9a8748b82be3062356e2533953f3029
yassminty <s10247871@connect.np.edu.sg>
2024-02-19 00:57:34 +0800

    idk :') added java but api not working idk :'))

--- script.js
+++ script.js
@@ -10,39 +10,43 @@ function menutoggle(){
     }
 }
 
-var RegForm = document.getElementById("RegForm");
+var RegForm = document.getElementById("register-form");
 
 
-function register(){
-    RegForm.style.transform = "translateX(0px)";
+function register(event){
+    event.preventDefault();
 }
 
+RegForm.addEventListener('submit', register)
 
 var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://spendersshoppes-3fdc.restdb.io/rest/contact",
-    "method": "GET",
-    "headers": {
-      "content-type": "application/json",
-      "x-apikey": "65c83936c8b8253b470d9e1f",
-      "cache-control": "no-cache"
-    }
+  "method": "GET",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "65c83936c8b8253b470d9e1f",
+    "cache-control": "no-cache"
   }
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+};
 
-  var jsondata = {"username": "email","password": "birthday"};
+fetch('https://spendersshoppes-3fdc.restdb.io/rest/contact', settings)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Log the data to the console for testing
+        // You can handle the API response data here
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 
+var jsondata = {"username": "email","password": "birthday"};
 
-  fetch('https://spendersshoppes-3fdc.restdb.io/home/db/spendersshoppes-3fdc')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // Log the data to the console for testing
-    // You can handle the API response data here
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
+// Fetching data from another endpoint
+fetch('https://spendersshoppes-3fdc.restdb.io/home/db/spendersshoppes-3fdc')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data); // Log the data to the console for testing
+        // You can handle the API response data here
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
